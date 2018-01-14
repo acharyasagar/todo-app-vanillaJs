6d155c5d5875c9cf1ad96282927d570afe3e11af
acharyasagar <acharyasaagar@gmail.com>
2018-01-14 12:02:42 -0800

    remove item and clear item functionality added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Define UI variables
+//Define UI variables and consts
 
 const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
@@ -13,33 +13,67 @@ loadEventListeners();
 function loadEventListeners(){
   //Add tasks event
   form.addEventListener('submit',addTask);
+  //Remove task event
+  taskList.addEventListener('click', removeTask);
+  //Add event to clear task button
+  clearBtn.addEventListener('click', clearTasks);
 }
 
 function addTask(e){
   e.preventDefault();
-  if(taskInput.value===''){
-    alert('Add a task');
+  //Check input and alert if its empty
+  if(taskInput.value === ''){
+
+  alert('Add a task');
   }
+  //If its not empty, add its value to the list
+  else{
 
-  //Create li element
-  const li = document.createElement('li');
-  //Add class to the li element
-  li.className='collection-item';
-  //Create textnode and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  //Create new link element
-  const link = document.createElement('a');
-  //Add class to the link element
-  link.className='delete-item secondary-content';
-  //Add icon to the link
-  link.innerHTML= '<i class="fa fa-remove"></i>';
-  //Now append the link to the li
-  li.appendChild(link);
-  console.log(li);
-  //Append the li to ul
-  taskList.appendChild(li);
+    //Create li element
+    const li = document.createElement('li');
+    //Add class to the li element
+    li.className='collection-item';
+    //Create textnode and append to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    //Create new link element
+    const link = document.createElement('a');
+    //Add class to the link element
+    link.className='delete-item secondary-content';
+    //Add icon to the link
+    link.innerHTML= '<i class="fa fa-remove"></i>';
+    //Now append the link to the li
+    li.appendChild(link);
+    //Append the li to ul
+    taskList.appendChild(li);
 
-  //Clear Input
-  taskInput.value = '';
+    //Clear Input
+    taskInput.value = '';
 
+  }
+}
+//Removetask function
+function removeTask(e){
+  //Uses Event delegation to target the i tag
+  if(e.target.parentElement.classList.contains('delete-item')){
+    if(confirm('Are you sure?')){
+      //removes the parent (li) of parent (a) tag
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+//Function clearTasks
+
+function clearTasks(e){
+  if(confirm('Are you sure you want to clear all tasks?')){
+
+      //taskList.innerHTML = ''; -- one way
+
+      //removing child using while loop
+
+      while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+      }
+
+  }
 }
